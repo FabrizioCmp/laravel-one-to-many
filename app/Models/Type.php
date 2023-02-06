e17554d5ec87d2ffbe2a6721fc1c66879591cfe2
FabrizioCmp <fabriziocampari01@gmail.com>
2023-02-06 14:04:00 +0100

    adds functions to Models

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Type extends Model
-{
+class Type extends Model{
     use HasFactory;
+
+    protected $fillable = [
+        "id","name"
+    ];
+
+    public function projects(){
+        return $this->hasMany(Type::class);  
+    }
 }
